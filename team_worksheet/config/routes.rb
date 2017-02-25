726c92985f9dc57e6321532bb331052919358d30
Aman Raghuvanshi <aman.raghuvanshi@gmail.com>
2017-02-24 21:05:19 -0800

    userlogin

--- team_worksheet/config/routes.rb
+++ team_worksheet/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'team/index'
 
   get 'team/index'
   root 'team#index'
+  get ':controller(/:action(/:id))(.:format)'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
