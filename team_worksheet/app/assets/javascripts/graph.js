fa44f4414e604a7665571759316457aebe3f6304
Daniel Lai <daniel.lai5896@gmail.com>
2017-04-05 18:27:26 -0700

    test

--- team_worksheet/app/assets/javascripts/graph.js
+++ team_worksheet/app/assets/javascripts/graph.js
@@ -10,6 +10,19 @@ $.ajax({
   }
 });
 
+var dataArray = [20, 40, 60];
+
 function draw(data) {
-  
-}
+  var container = d3.select("body")
+                    .append("svg")
+                    .attr("width", 500)
+                    .attr("height", 500);
+
+  var bars = container.selectAll("rect")
+                      .data("dataArray")
+                      .enter()
+                        .append("rect")
+                        .attr("width", function(d) {return d * ;})
+                        .attr("height", 50);
+                        .attr("y", function(d, i) {return d * i});
+  }
