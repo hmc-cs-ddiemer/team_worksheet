5b4ca33b83bfbe32add15779b42f463dac96264d
Daniel Lai <daniel.lai5896@gmail.comm>
2017-04-21 13:31:28 -0700

    Creating x axis sections

--- team_worksheet/app/controllers/graph_controller.rb
+++ team_worksheet/app/controllers/graph_controller.rb
@@ -4,9 +4,7 @@ class GraphController < ApplicationController
 
   def data
     respond_to do |format|
-      format.json {
-        render :json => [1,2,3,4,5]
-      }
+      format.json {}
     end
   end
 end
